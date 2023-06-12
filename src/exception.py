537cd50e68a82fffbc33567c2c348956b5e8f76b
BilalDevRepo <bilalk2907@gmail.com>
2023-06-12 23:51:49 +0530

    Hyperparameter Training

--- src/exception.py
+++ src/exception.py
@@ -8,6 +8,7 @@ def error_message_detail(error,error_detail:sys):
      file_name,exc_tb.tb_lineno,str(error))
 
     return error_message
+
     
 
 class CustomException(Exception):
@@ -18,21 +19,6 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
 
-
-
-"""
-
-if __name__=="__main__":
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info("Divide by zero error!")
-        raise CustomException(e,sys)
-    
-
-    
-
-"""
 
 
 
